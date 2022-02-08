9a20fa08f60824ea2a5500a4533d44c170ea3061
ji-web <81374830+ji-web@users.noreply.github.com>
2022-02-08 20:46:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var song1="";
+var song2="";
+left_wristX=0;
+left_wristY=0;
+right_wristX=0;
+right_wristY=0;
+
+function preload(){
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modalLoaded);
+    poseNet.on("pose",gotPoses);
+
+}
+
+function draw(){
+    image(video,0,0,500,500);
+}
+
+function modalLoaded(){
+    console.log("poseNet is loaded"); 
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        left_wristX=results[0].pose.leftWrist.x;
+        left_wristY=results[0].pose.leftWrist.y;
+        console.log("left wrist X="+left_wristX+"left wrist Y="+left_wristY);
+        right_wristX=results[0].pose.rightWrist.x;
+        right_wristY=results[0].pose.rightWrist.y;
+        console.log("right wrist X="+right_wristX+"right wrist Y="+right_wristY);
+    }
+}
+
+
+
